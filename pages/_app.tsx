ef41567c30db6906d1a1a7d3a425a169615ce21f
hong <bkn367@naver.com>
2023-05-12 23:37:13 +0900

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Nunito } from "next/font/google";
+import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
+
+const font = Nunito({
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={font.className}>
+      <ClientOnly>
+        <Modal actionLabel="Submit" title="Hello World" isOpen />
+        <Navbar />
+      </ClientOnly>
+      <Component {...pageProps} />
+    </main>
+  );
 }
---
+++ pages/components/navbar/MenuItem.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+interface MenuItemProps {
+  onClick: () => void;
+  label: string;
+}
+const MenuItem: React.FC<MenuItemProps> = ({ onClick, label }) => {
+  return (
+    <div
+      className="
+        onClick={onClick}
+        px-4
+        py-3
+        hover:bg-neutral-100
+        transition
+        font-semibold
+    "
+    >
+      {label}
+    </div>
+  );
+};
+
+export default MenuItem;
